782241ba6d2374c455fe2c5e60ad39933e10184a
vincentowler <vincentowler@outlook.com>
2023-01-25 11:57:44 +0000

    Add test

--- tests/books.spec.ts
+++ tests/books.spec.ts
@@ -1,6 +1,12 @@
 import test, { expect } from "@playwright/test";
 
-test("should display a list of books", async ({ page }) => {
+test("should display a list of books and support deleting all books", async ({
+  page,
+}) => {
   await page.goto("http://localhost:5173/books");
   await expect(page.getByText("The Design of Everyday Things")).toHaveCount(1);
+  await page
+    .getByRole("button", { name: "Delete The Design of Everyday Things" })
+    .click();
+  await expect(page.getByText("The Design of Everyday Things")).toHaveCount(0);
 });
