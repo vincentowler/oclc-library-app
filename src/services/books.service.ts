f6076202b3a49d97e69a24a63915397e95eea7d9
vincentowler <vincentowler@outlook.com>
2023-01-26 11:13:29 +0000

    add

--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -1,4 +1,4 @@
-import { Book } from "../Books";
+import { Book, NewBook } from "../Books";
 
 export async function getBooks(): Promise<Book[]> {
   const response = await fetch("http://localhost:3001/books");
@@ -11,4 +11,16 @@ export async function deleteBook(id: number): Promise<void> {
     method: "DELETE",
   });
   if (!response.ok) throw new Error(response.statusText);
+}
+
+export async function addBook(newBook: NewBook): Promise<Book> {
+  const response = await fetch("http://localhost:3001/books", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newBook),
+  });
+  if (!response.ok) throw new Error(response.statusText);
+  return response.json();
 }
