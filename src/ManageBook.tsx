0f2170e0e47520669d5c573aba5291241bf799b3
vincentowler <vincentowler@outlook.com>
2023-01-26 11:33:24 +0000

    onChange

--- src/ManageBook.tsx
+++ src/ManageBook.tsx
@@ -1,19 +1,34 @@
 import { Button, TextField } from "@mui/material";
 import { Stack } from "@mui/system";
+import { useState } from "react";
+import { NewBook } from "./Books";
+import { addBook } from "./services/books.service";
 
 export default function ManageBook() {
+  const [book, setBook] = useState<NewBook>({
+    title: "",
+    subject: "",
+  });
+
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setBook({
+      ...book,
+      [event.target.name]: event.target.value,
+    });
+  }
+
   return (
     <>
       <h1>Add Book</h1>
 
       <form
         onSubmit={() => {
-          //
+          addBook(book);
         }}
       >
         <Stack spacing={2}>
-          <TextField label="Title" />
-          <TextField label="Subject" />
+          <TextField label="Title" value={book.title} onChange={onChange} />
+          <TextField label="Subject" value={book.subject} onChange={onChange} />
           <Button type="submit" variant="contained">
             Save
           </Button>
