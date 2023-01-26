9f0261ab1275b42bffd0623e3b16dd5a5b2da87a
vincentowler <vincentowler@outlook.com>
2023-01-26 13:23:46 +0000

    add test

--- tests/books.spec.ts
+++ tests/books.spec.ts
@@ -1,12 +1,9 @@
-import test, { expect } from "@playwright/test";
+import test, { expect, Page } from "@playwright/test";
 
-test("should display a list of books and support deleting all books", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:5173");
-  const book1Title = "The Design of Everyday Things";
-  const book2Title = "The Most Human Human";
+const book1Title = "The Design of Everyday Things";
+const book2Title = "The Most Human Human";
 
+async function checkForBooks(page) {
   // Locator
   const book1 = page.getByText(book1Title);
   const book2 = page.getByText(book2Title);
@@ -18,21 +15,16 @@ test("should display a list of books and support deleting all books", async ({
   await expect(page.getByRole("progressbar")).toHaveCount(0);
 
   await expect(book1).toHaveCount(1);
-  await page.getByRole("button", { name: "Delete " + book1Title }).click();
-  await expect(book1).toHaveCount(0);
-  await expect(page.getByText("Book deleted.")).toHaveCount(1);
-
   await expect(book2).toHaveCount(1);
-  await page.getByRole("button", { name: "Delete " + book2Title }).click();
-  await expect(book2).toHaveCount(0);
+}
+
+async function deleteBook(page: Page, bookTitle: string) {
+  await page.getByRole("button", { name: "Delete " + bookTitle }).click();
+  await expect(page.getByText(bookTitle)).toHaveCount(0);
   await expect(page.getByText("Book deleted.")).toHaveCount(1);
+}
 
-  // All books should now be deleted so this message should display
-  await expect(page.getByText("No books in the library.")).toHaveCount(1);
-});
-
-test("should support adding a new book", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+async function addBook(page: Page, bookTitle: string, bookSubject: string) {
   await page.getByRole("link", { name: "Add Book" }).click();
 
   // now the URL should be manage-book
@@ -41,14 +33,27 @@ test("should support adding a new book", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Add Book" })).toHaveCount(1);
 
   // Fill out the form
-  await page.getByLabel("Title").fill("New Book");
-  await page.getByLabel("Subject").fill("New Subject");
+  await page.getByLabel("Title").fill(bookTitle);
+  await page.getByLabel("Subject").fill(bookSubject);
   await page.getByRole("button", { name: "Save" }).click();
 
   // Should be redirected to the home page
   await expect(page).toHaveURL("http://localhost:5173/");
   await expect(page.getByText("New Book")).toHaveCount(1);
   await expect(page.getByText("New Subject")).toHaveCount(1);
+}
+
+test("should display books, support adding, and deleting", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+  await checkForBooks(page);
+
+  // Delete
+  await deleteBook(page, book1Title);
+  await deleteBook(page, book2Title);
+  await expect(page.getByText("No books in the library.")).toHaveCount(1);
+
+  addBook(page, book1Title, "Design");
+  addBook(page, book2Title, "Computer Science");
 });
 
 test("should support navigating between pages via the navbar", async ({
