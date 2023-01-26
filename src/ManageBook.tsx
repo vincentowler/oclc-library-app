25d6016ab2e71f52940dc39a4e6c0cedeabba28f
vincentowler <vincentowler@outlook.com>
2023-01-26 14:00:21 +0000

    validation

--- src/ManageBook.tsx
+++ src/ManageBook.tsx
@@ -5,8 +5,13 @@ import { useNavigate } from "react-router-dom";
 import { NewBook } from "./Books";
 import { addBook } from "./services/books.service";
 
+type Status = "idle" | "submitting" | "submitted";
+
+type Errors = Partial<NewBook>;
+
 export default function ManageBook() {
   const navigate = useNavigate();
+  const [status, setStatus] = useState<Status>("idle");
   const [book, setBook] = useState<NewBook>({
     title: "",
     subject: "",
@@ -19,6 +24,20 @@ export default function ManageBook() {
     });
   }
 
+  function getErrors() {
+    const errors: Errors = {};
+    if (!book.title) {
+      errors.title = "Title is required";
+    }
+    if (!book.subject) {
+      errors.subject = "Subject is required";
+    }
+    return errors;
+  }
+
+  // Derived state
+  const errors = getErrors();
+
   return (
     <>
       <h1>Add Book</h1>
@@ -26,6 +45,12 @@ export default function ManageBook() {
       <form
         onSubmit={async (event) => {
           event.preventDefault(); // prevent posting to server
+          setStatus("submitting");
+          const errorsExist = Object.keys(errors).length > 0;
+          if (errorsExist) {
+            setStatus("submitted");
+            return;
+          }
           await addBook(book);
           navigate("/");
         }}
@@ -36,12 +61,16 @@ export default function ManageBook() {
             label="Title"
             value={book.title}
             onChange={onChange}
+            error={status !== "idle" && Boolean(errors.title)}
+            helperText={status !== "idle" && errors.title}
           />
           <TextField
             name="subject"
             label="Subject"
             value={book.subject}
             onChange={onChange}
+            error={status !== "idle" && Boolean(errors.subject)}
+            helperText={status !== "idle" && errors.subject}
           />
           <Button type="submit" variant="contained">
             Save
