200cc2deef8b23a0a3d20ba6878ae05b1be100db
vincentowler <vincentowler@outlook.com>
2023-01-26 11:13:46 +0000

    create form

--- src/ManageBook.tsx
+++ src/ManageBook.tsx
@@ -1,3 +1,24 @@
+import { Button, TextField } from "@mui/material";
+import { Stack } from "@mui/system";
+
 export default function ManageBook() {
-  return <h1>Add Book</h1>;
+  return (
+    <>
+      <h1>Add Book</h1>
+
+      <form
+        onSubmit={() => {
+          //
+        }}
+      >
+        <Stack spacing={2}>
+          <TextField label="Title" />
+          <TextField label="Subject" />
+          <Button type="submit" variant="contained">
+            Save
+          </Button>
+        </Stack>
+      </form>
+    </>
+  );
 }
